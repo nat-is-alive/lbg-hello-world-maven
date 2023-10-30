d5d15e3ad09ed67b8dacec15f8bceafc7fbcb072
Nathaniel Usherwood <nathanielusherwood@Nathaniels-MBP.lan>
2023-10-30 13:55:23 +0000

    added name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -4,7 +4,7 @@ package com.qa;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -12,6 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Nat"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
